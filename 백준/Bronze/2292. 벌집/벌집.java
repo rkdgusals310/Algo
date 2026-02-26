d946651567bff01e0ae2b4c437b1b99bdeabd51f
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-02-26 17:41:59 +0900

    [Bronze II] Title: 벌집, Time: 168 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,17 +1,19 @@
-import java.util.*;
 
-public class Main{
-    public static void main(String args[]){
-        Scanner s=new Scanner(System.in);
-        
-        int n=s.nextInt();
-        int num=1;
-        int cnt=1;
-        while(true){
-            if(n<=num){
-                break;
-            }num+=6*cnt;cnt++;
-        }
-        System.out.print(cnt);
-    }
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		long n=s.nextInt();
+		int cnt=0;
+		long start=0;
+		long nn=1;
+		int i=0;
+		while(true) {
+			start=nn+6*i;
+			nn=start;
+			i++;
+			if(start>=n||n==1) {System.out.println(i);break;}
+		}
+	}
 }
