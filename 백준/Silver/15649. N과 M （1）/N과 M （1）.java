74cbae8c1a717215d3c2ff5322b81915f068b24b
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-02-27 12:04:18 +0900

    [Silver III] Title: N과 M (1), Time: 308 ms, Memory: 26300 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main{
+    public static StringBuilder sb=new StringBuilder();
+    public static int arr[];
+    public static boolean visited[];
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        
+        int n=s.nextInt();
+        int m=s.nextInt();
+        arr=new int[m];
+        visited=new boolean[n];
+        //dfs 사용해야함
+        dfs(n,m,0);
+        System.out.print(sb);
+    }
+    
+    public static void dfs(int n,int m,int depth){
+        if(depth==m){
+            for(int i=0;i<m;i++){
+                sb.append(arr[i]).append(" ");
+            }sb.append("\n");
+            return;
+        }
+        
+        for(int i=0;i<n;i++){
+            
+            if(!visited[i]){
+                visited[i]=true;
+                arr[depth]=i+1;
+                dfs(n,m,depth+1);
+                visited[i]=false;
+            }
+            
+            
+            
+
+        }
+    }
+}
