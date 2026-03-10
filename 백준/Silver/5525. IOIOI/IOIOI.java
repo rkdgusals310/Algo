8325352565f67fcffeb147e9e1d74aeb62d36183
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-03-10 13:58:47 +0900

    [Silver I] Title: IOIOI, Time: 204 ms, Memory: 20380 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+/*
+    문제를 읽고 바로 생각나는 방법은 단순비교
+    최대 입력값이 크기 때문에 반복문 한번만 사용해야함
+
+*/
+public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        
+        int n=sc.nextInt();
+        int cnt=0;
+        String str="IOI";
+        for(int i=1;i<n;i++){
+            str+="OI";
+        }
+        
+        int m=sc.nextInt();
+        String s=sc.next();
+
+        for(int i=0;i<=m-str.length();i++){
+            if(s.substring(i,i+str.length()).equals(str))cnt++;
+        }
+        
+        System.out.print(cnt);
+    }
+}
