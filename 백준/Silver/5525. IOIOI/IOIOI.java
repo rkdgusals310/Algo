51f9592b4441f8f538e52e021ee34f6e9702275a
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-03-10 14:48:46 +0900

    [Silver I] Title: IOIOI, Time: 212 ms, Memory: 20132 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,25 +1,18 @@
 import java.util.*;
 
-
-/*
-    문제를 읽고 바로 생각나는 방법은 단순비교
-    최대 입력값이 크기 때문에 반복문 한번만 사용해야함
-
-*/
 public class Main{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         
         int n=sc.nextInt();
-        if(n<1||n>100)return;
+        int m=sc.nextInt();
+        
         int cnt=0;
         String str="IOI";
         for(int i=1;i<n;i++){
             str+="OI";
         }
         
-        int m=sc.nextInt();
-        if(m<(2*n+1)||n>10000)return;
         String s=sc.next();
 
         for(int i=0;i<=m-str.length();i++){
