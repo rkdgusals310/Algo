15443b2008b2a5ef2360cccedfeb2a8de352be4b
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-03-30 21:38:00 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 18752 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,38 +1,32 @@
 import java.util.*;
 
-public class Main{
-    public static int arr[];
-    public static StringBuilder sb=new StringBuilder();
-    public static void main(String args[]){
-        Scanner s=new Scanner(System.in);
-        
-        int n=s.nextInt();
-        int m=s.nextInt();
-        
-        arr=new int[m];
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
 
-        dfs(n,m,0);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[M];
+
+        dfs(0, 1);
         System.out.print(sb);
     }
-    public static void dfs(int n,int m,int depth){
-        if(depth==m){
-            for(int i=0;i<m;i++){
-                sb.append(arr[i]).append(" ");
-            }sb.append("\n");
+
+    static void dfs(int depth, int start) {
+        if (depth == M) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
             return;
         }
-        
-        for(int i=0;i<n;i++){
-            if(depth!=0&&arr[depth-1]<=i+1){
-                arr[depth]=i+1;
-                dfs(n,m,depth+1);
-            }
-            else if(depth==0){
-                arr[depth]=i+1;
-                dfs(n,m,depth+1);
-            }
-        }
 
+        for (int i = start; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1, i); 
+        }
     }
 }
-
