06a16aaf2f1260cfbce27df17d29b75d142ba57d
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-03-30 15:40:08 +0900

    [Silver III] Title: N과 M (3), Time: 488 ms, Memory: 71336 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,34 @@
+// 순열 문제인데 중복가능하다. 
+import java.util.*;
+
+public class Main{
+    public static int arr[];
+    public static boolean visited[];
+    public static StringBuilder sb=new StringBuilder();
+    
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        
+        int n=s.nextInt();
+        int m=s.nextInt();
+        
+        arr=new int[m];
+        visited=new boolean[n];
+        
+        dfs(n,m,0);
+        System.out.print(sb);
+    }
+    public static void dfs(int n,int m,int depth){
+        if(depth==m){
+            for(int i=0;i<depth;i++){
+                sb.append(arr[i]).append(" ");
+            }sb.append("\n");
+            return;
+        }
+        for(int i=0;i<n;i++){
+            arr[depth]=i+1;
+            dfs(n,m,depth+1);
+        }
+        
+    }
+}
