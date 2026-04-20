757743a729765b295f476560af8a2eff895ddaba
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-04-20 19:12:15 +0900

    [Silver III] Title: 1로 만들기, Time: 412 ms, Memory: 77028 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,30 +1,22 @@
 import java.util.*;
 
-
 public class Main{
     public static void main(String args[]){
         Scanner s=new Scanner(System.in);
         
         int n=s.nextInt();
-
-        int dp[]=new int[n+3];
-        dp[1]=0;
-        dp[2]=1;
-        dp[3]=1;
+        int arr[]=new int[n+1];
+        System.out.print(dp(n,arr));
+    }
+    public static int dp(int n,int arr[]){
+        if(arr[n]!=0)return arr[n];
+        if(n==1)return arr[1]=0;
+        if(n<4)return arr[n]=1;
         
-        for(int i=4;i<=n;i++){
-          dp[i]=dp[i-1];
-          
-            if(i%2==0){
-                dp[i]=dp[i/2]>dp[i]?dp[i]:dp[i/2];
-            }
-            if(i%3==0){
-                dp[i]=dp[i/3]>dp[i]?dp[i]:dp[i/3];
-            }
-            dp[i]+=1;
-
-        }
-            
-        System.out.print(dp[n]);
+        arr[n]=dp(n-1,arr);
+        if(n%2==0)arr[n]=arr[n]>dp(n/2,arr)?dp(n/2,arr):arr[n];
+        if(n%3==0)arr[n]=arr[n]>dp(n/3,arr)?dp(n/3,arr):arr[n];
+        
+        return arr[n]=arr[n]+1;
     }
 }
