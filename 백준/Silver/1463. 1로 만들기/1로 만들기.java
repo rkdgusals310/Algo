110c48722333485eb3d84f2b936ce0868e35098e
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-04-20 16:33:16 +0900

    [Silver III] Title: 1로 만들기, Time: 204 ms, Memory: 21616 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,19 +1,30 @@
-import java.util.Scanner;
- 
-public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		System.out.println(recur(N, 0));
-	}
- 
-	static int recur(int N, int count) {
-		// N이 2 미만인 경우 누적된 count값을 반환
-		if (N < 2) {
-			return count;
-		}
-		return Math.min(recur(N / 2, count + 1 + (N % 2)), recur(N / 3, count + 1 + (N % 3)));
- 
-	}
+import java.util.*;
+
+
+public class Main{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        
+        int n=s.nextInt();
+
+        int dp[]=new int[n+3];
+        dp[1]=0;
+        dp[2]=1;
+        dp[3]=1;
+        
+        for(int i=4;i<=n;i++){
+          dp[i]=dp[i-1];
+          
+            if(i%2==0){
+                dp[i]=dp[i/2]>dp[i]?dp[i]:dp[i/2];
+            }
+            if(i%3==0){
+                dp[i]=dp[i/3]>dp[i]?dp[i]:dp[i/3];
+            }
+            dp[i]+=1;
+
+        }
+            
+        System.out.print(dp[n]);
+    }
 }
