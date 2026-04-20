3a44076d6343a070b5306aabd5d7919fc2d1c731
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-04-20 12:14:21 +0900

    [Silver IV] Title: 동전 0, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main{
     public static void main(String args[]){
         Scanner s=new Scanner(System.in);
+        
         int n=s.nextInt();
         int k=s.nextInt();
-        int num[]=new int[n];
         int cnt=0;
+        int arr[]=new int[n];
         for(int i=0;i<n;i++){
-            num[i]=s.nextInt(); 
+            arr[i]=s.nextInt();
         }
         for(int i=n-1;i>=0;i--){
-            if(k/num[i]>0){
-                cnt+=k/num[i];
-                k=k%num[i];
-            }
+            cnt+=k/arr[i];
+            k=k%arr[i];
         }
         System.out.print(cnt);
-        
     }
 }
