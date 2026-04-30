8f87fbca32e826c8ec5de4fbf3add4686d6d26b2
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-04-30 20:00:33 +0900

    [level 3] Title: 단속카메라, Time: 11.41 ms, Memory: 55.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,34 @@
+/*
+고속도로 이용 모든 차량이 단속용 카메라를 한번은 만나도록 카메라 설치하려고함
+차량의 경로 routes
+최소 몇대의 카메라가 설치되어야하는지 return,
+
+
+*/
+import java.util.*;
+class Solution {
+public int solution(int[][] routes) {
+    List<Integer> list=new ArrayList<>();
+
+
+    Arrays.sort(routes, (o1, o2) -> {
+    if (o1[1] == o2[1]) {
+        return Integer.compare(o1[0], o2[0]); // 2순위
+    }
+    return Integer.compare(o1[1], o2[1]); // 1순위
+    });
+    int answer = 0;
+
+    list.add(routes[0][1]);
+    
+    for(int i=1;i<routes.length;i++){
+        int size=list.size();
+        if(list.get(size-1)<routes[i][0]){
+            list.add(routes[i][1]);
+        }
+    }
+
+
+    return answer=list.size();
+}
+}
