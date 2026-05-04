dbc05e4de4c8134588eb9118f675c58b2b742913
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-04 21:54:50 +0900

    [D2] Title: 간단한 369게임, Time: 96 ms, Memory: 27,136 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner s = new Scanner(System.in);
+
+
+            int n=s.nextInt();
+            threeSixNine(n);
+	}
+    public static void threeSixNine(int n){
+        StringBuilder sb=new StringBuilder();
+        for(int i=1;i<=n;i++){
+            String str=""+i;
+           
+            if (str.contains("3") || str.contains("6") || str.contains("9")){
+                
+            	for(int j=0;j<str.length();j++){
+                    String num=""+str.charAt(j);
+                	if("369".contains(num)){sb.append("-");}
+                }sb.append(" ");
+            }
+            else{sb.append(str+" ");}
+        }System.out.println(sb);
+    }
+}
