a045bf2eea4a15f0f61e72d64f657fefcfe4f883
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-11 13:45:42 +0900

    [Passed] 점수 입출력 (130ms, 10MB)

---
+++ Codetree/trail0/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s=new Scanner(System.in);
+
+        int n=s.nextInt();
+        System.out.printf("Your score is %d point.",n);
+    }
+}
