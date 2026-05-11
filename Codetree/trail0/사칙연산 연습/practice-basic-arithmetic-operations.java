ed3028d96835b0bf3d4ea6be0250d63124c72cd7
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-11 13:11:36 +0900

    [Passed] 사칙연산 연습 (95ms, 9MB)

---
+++ Codetree/trail0/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=5;
+        int b=30;
+        int c=a+b;
+
+        System.out.print((c/8)+"\n"+(c%8));
+    }
+}
