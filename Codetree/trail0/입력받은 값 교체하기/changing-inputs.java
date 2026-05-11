5a02a5510aacef83aedd4e6ea73d41586bca7d36
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-11 14:15:57 +0900

    [Passed] 입력받은 값 교체하기 (128ms, 10MB)

---
+++ Codetree/trail0/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.    
+        Scanner s=new Scanner(System.in);
+
+        int a=s.nextInt();
+        int b=s.nextInt();
+
+        System.out.print(b+" "+a);
+
+    }
+}
