66c82b2756742ee92dc4345ec7aef1148f0c6153
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-11 13:51:13 +0900

    [Passed] 실수 받아 그대로 출력 (168ms, 25MB)

---
+++ Codetree/trail0/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s=new Scanner(System.in);
+
+        float f=s.nextFloat();
+        System.out.print(Math.round(f*100)/100.0);
+    }
+}
