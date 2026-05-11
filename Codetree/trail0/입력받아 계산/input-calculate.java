4675d51789723c98611f629003ac8076dbd23009
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-11 13:46:48 +0900

    [Passed] 입력받아 계산 (130ms, 10MB)

---
+++ Codetree/trail0/입력받아 계산/input-calculate.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s=new Scanner(System.in);
+
+        int n=s.nextInt();
+        System.out.print(n+2);
+    }
+}
