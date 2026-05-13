bed443fc187c5c7de6814b9309731ea561ff74d8
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-13 16:39:39 +0900

    [Passed] 합과 평균 (133ms, 10MB)

---
+++ Codetree/trail0/합과 평균/sum-and-avg.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner s=new Scanner(System.in);
+
+        int a=s.nextInt();
+        int b=s.nextInt();
+
+        System.out.printf("%d %.1f",a+b,(a+b)/2.0);
+    }
+
+}
