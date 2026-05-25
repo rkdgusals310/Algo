64015da0ba9fccc8967499c2b565bf0302a4793a
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-25 16:02:17 +0900

    [level 1] Title: K번째수, Time: 0.29 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,21 +1,20 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
+        
         for(int i=0;i<commands.length;i++){
-            int temp[]=new int[commands[i][1]-commands[i][0]+1];
-            int index=0;
-            
-            for(int j=commands[i][0]-1;j<commands[i][1];j++){
-                temp[index]=array[j];
-                index++;
+            int arr[]=new int[commands[i][1]-commands[i][0]+1];
+            int cnt=0;
+            for(int j=commands[i][0];j<=commands[i][1];j++){
+                arr[cnt]=array[j-1];
+                cnt++;
             }
-            Arrays.sort(temp);
-            answer[i]=temp[commands[i][2]-1];
+            Arrays.sort(arr);
+            answer[i]=arr[commands[i][2]-1];
         }
-
+        
+        
         return answer;
     }
 }
