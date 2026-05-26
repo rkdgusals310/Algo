0a2bc1f7a49963e6d4c073ca1a527e8a1c851eba
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-26 16:38:35 +0900

    [level 2] Title: 가장 큰 수, Time: 1607.46 ms, Memory: 370 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,20 +2,20 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        Integer[] arr = new Integer[numbers.length];
+        String[] arr = new String[numbers.length];
         
         for(int i=0;i<numbers.length;i++){
-            arr[i]=numbers[i];
+            arr[i]=""+numbers[i];
         }
         
         Arrays.sort(arr,(o1,o2)->{
-            String s1=String.valueOf(o1)+String.valueOf(o2);
-            String s2=String.valueOf(o2)+String.valueOf(o1);
+            String s1=o1+o2;
+            String s2=o2+o1;
 
-            return Integer.compare(Integer.parseInt(s1),Integer.parseInt(s2));
+            return s1.compareTo(s2);
 
         });
-        if(arr[numbers.length-1]==0) return "0";
+        if(arr[numbers.length-1].equals("0")) return "0";
         for(int i=numbers.length-1;i>=0;i--)answer+=arr[i];
         return answer;
     }
