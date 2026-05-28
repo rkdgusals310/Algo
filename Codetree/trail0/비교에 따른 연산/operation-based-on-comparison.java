d22e7203c83015f6d00a92e333541299fc8559f9
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-28 23:55:05 +0900

    [Passed] 비교에 따른 연산 (127ms, 10MB)

---
+++ Codetree/trail0/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+
+        int a=s.nextInt();
+        int b=s.nextInt();
+
+        System.out.print(a>b?a*b:b/a);
+    }
+}
