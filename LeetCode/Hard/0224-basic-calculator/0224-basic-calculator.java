4f235627bddf371dbd9b0eb316fecf0539211ec6
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-05-29 20:03:33 +0900

    117 ms (5.06%) 87.7 MB (5.28%) 0224-basic-calculator .java Hard Math

---
+++ LeetCode/Hard/0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,74 @@
+class Solution {
+    public int calculate(String s) {
+        Queue<String> q=new LinkedList<>();
+        Stack<String> stack=new Stack<>();
+        Stack<String> temp=new Stack<>();
+
+        String n="";
+        int cnt=0;
+        for(String str:s.split("")){
+            if(str.equals(" "))continue;
+
+            if(!str.equals("(")&&!str.equals(")")&&!str.equals("-")&&!str.equals("+")){
+                n+=str;
+                cnt++;
+            }
+            else{
+                if(cnt!=0){
+                    q.offer(n);
+                    cnt=0;
+                    n="";
+                }
+                q.offer(str);
+            }
+        }
+        if(cnt!=0)q.offer(n);
+        while(!q.isEmpty()){
+            String basic=q.poll();
+            if(basic.equals(")")){
+                while(true){
+                    String t=stack.pop();
+                    if(t.equals("(")){
+                        stack.push(cal(temp));
+                        break;
+                    }
+                    temp.push(t);
+                }
+                
+            }
+            else {
+                stack.push(basic);
+            }
+        }
+        while(!stack.isEmpty())temp.push(stack.pop());
+        return Integer.parseInt(cal(temp));
+
+    }
+    public String cal(Stack<String> stack){
+        long num=0;
+        String op="*";
+
+        while(!stack.isEmpty()){
+            String ch=stack.pop();
+            
+            if(ch.equals("+")){
+                op="+";
+            }
+            else if(ch.equals("-")){
+                op="-";
+            }
+            else{
+                if(op.equals("*")){
+                    num=Long.parseLong(ch);
+                }
+                else{
+                    if(op.equals("+"))num+=Long.parseLong(ch);
+                    else num-=Long.parseLong(ch);
+                    op="*";
+                }
+            }
+        }
+
+        return ""+num;
+    }
+}
