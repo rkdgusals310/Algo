283b7d47f2bb95f6e7e3ca1e9f372272f9795ff9
HyeonMInKang <66188909+rkdgusals310@users.noreply.github.com>
2026-07-30 19:02:22 +0900

    [level 0] Title: 외계어 사전, Time: 0.70 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,19 +1,6 @@
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        int answer = 2;
-        
-        for(String str:dic){
-            int cnt=spell.length;
-            for(String s:spell){
-                if(str.contains(s))cnt--;
-            }
-            if(cnt==0){
-                System.out.println(str);
-                answer=1;
-                break;
-            }
-        }
-        
+        int answer = 0;
         return answer;
     }
 }
